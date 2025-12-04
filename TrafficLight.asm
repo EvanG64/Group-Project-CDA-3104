22aac34f5a899208427a9913f27decce7f80ff6f
yudintsevtimofey <yudintsevtimofey@gmail.com>
2025-12-03 19:22:30 -0500

    Very close at finishing comments

--- TrafficLight.asm
+++ TrafficLight.asm
@@ -9,7 +9,11 @@
 ; declare constants and global variables
 ; ----------------------------------------------------------
                ; us  *  XTAL / scaler - 1
-.equ DELAY_MS = 90000 * (16 / 256.0) - 1
+.equ DELAY_MS = 90000 * (16 / 256.0) - 1          ; setting delay
+
+
+; ----------------------------------------------------------
+; All of these are just variables to make it easier to read
 
 .equ RED_N_PIN = PB4
 .equ YELLOW_N_PIN = PB3
@@ -30,6 +34,8 @@
 .equ CROSS_N_BTN = PD3
 .equ CROSS_W_BTN = PD2
 
+
+; flags for crosswalk buttons
 .def crossNFlag = r21
 .def crossWFlag = r22
 
@@ -87,18 +93,26 @@ main:
 
           sei                           ; turn global interrupts on
 
+
+          ; making flags start at false
           ldi       crossWFlag, 0
           ldi       crossNFlag, 0
 
+
+; ----------------------------------------------------------
+; main loop of the traffic light
 main_loop:
 
 ; ----------------------------------------------------------
+; all red
           ldi       r16, 25
           call      n_red_w_red
           call      n_cross_red
           call      w_cross_red
           call      delay_lp
 ; ----------------------------------------------------------
+; north road is green, west red, and crosswalk on west is on
+; if it was called
           ldi       r16, 75
           call      n_green_w_red
           tst       crossWFlag
@@ -107,6 +121,7 @@ main_loop:
 no_w_cross_white_1:
           call      delay_lp
 ; ----------------------------------------------------------
+; preparing to stop
           ldi       r16, 25
           call      n_yellow_w_red
           tst       crossWFlag
@@ -116,12 +131,15 @@ no_w_cross_white_2:
           call      delay_lp
           ldi       crossWFlag, 0
 ; ----------------------------------------------------------
+; all red
           ldi       r16, 25
           call      n_red_w_red
           call      n_cross_red
           call      w_cross_red
           call      delay_lp
 ; ----------------------------------------------------------
+; north road is red, west green, and crosswalk on north is on
+; if it was called
           ldi       r16, 75
           call      n_red_w_green
           tst       crossNFlag
@@ -130,6 +148,7 @@ no_w_cross_white_2:
 no_n_cross_white_1:
           call      delay_lp
 ; ----------------------------------------------------------
+; preparing to stop
           ldi       r16, 25
           call      n_red_w_yellow
           tst       crossNFlag
@@ -140,8 +159,6 @@ no_n_cross_white_2:
           ldi       crossNFlag, 0
 ; ----------------------------------------------------------
 
-         
-
 end_main:
           rjmp main_loop
 
@@ -149,36 +166,28 @@ end_main:
 
 
 ; ----------------------------------------------------------
+; this loop handles delay and croswalk blinking if needed
 
 delay_lp:                               ; do {
-          
+          call      delay               ; calling delay of 0.09 ms
 
-          call      delay
+          cpi       r16, 10             ; more than halfway through yellow light, start blinking on crosswalk
+          brge      no_blink_white      
 
-          cpi       r16, 10
-          brge      no_blink_white
-
-          sbic      WEST, YELLOW_W_PIN
-          call      n_cross_white_blink
-          sbic      NORTH, YELLOW_N_PIN
-          call      w_cross_white_blink
-
-
-
-
+          sbic      WEST, YELLOW_W_PIN  ; making sure light is yellow for blink to start on West, skip next line if not
+          call      n_cross_white_blink ; calling blink function for west
+          sbic      NORTH, YELLOW_N_PIN ; making sure light is yellow for blink to start on North, skip next line if not
+          call      w_cross_white_blink ; calling blink function for north
 
 no_blink_white:
-
           dec       r16                 ;   --r16
           brne      delay_lp            ; } while (r16 > 0);
-
-          
-          ret      
-
-
+                   
+          ret                           ; delay_lp
 
 
 ; ----------------------------------------------------------
+; delay, which is set as 0.09 ms at the very top
 delay:
           ; Load TCNT1H:TCNT1L with initial count
           clr       r20
@@ -215,22 +224,71 @@ Monitor_OCF1A:
           ret                           ; delay
 
 
+; ----------------------------------------------------------
+; this is all the lights functions, basically on/off switches
+
+
+n_green_w_red:
+          sbi       NORTH, GREEN_N_PIN            ; turn green north on
+          sbi       WEST, RED_W_PIN               ; turn red west on
+          cbi       NORTH, RED_N_PIN              ; turn red north off
+          cbi       NORTH, YELLOW_N_PIN           ; turn yellow north off
+          cbi       WEST, YELLOW_W_PIN            ; turn yellow west off
+          cbi       WEST, GREEN_W_PIN             ; turn green west off
+          
+          ret       ; n_green_w_red
+n_red_w_green:
+          sbi       NORTH, RED_N_PIN              ; turn red north on
+          sbi       WEST, GREEN_W_PIN             ; turn green west on
+          cbi       NORTH, YELLOW_N_PIN           ; turn yellow north off
+          cbi       NORTH, GREEN_N_PIN            ; turn green north off
+          cbi       WEST, RED_W_PIN               ; turn red west off
+          cbi       WEST, YELLOW_W_PIN            ; turn yellow west off
+
+          ret      ; n_red_w_green
+n_yellow_w_red:
+          sbi       NORTH, YELLOW_N_PIN           ; turn yellow north on
+          sbi       WEST, RED_W_PIN               ; turn red west on
+          cbi       NORTH, RED_N_PIN              ; turn red north off
+          cbi       NORTH, GREEN_N_PIN            ; turn green north off
+          cbi       WEST, YELLOW_W_PIN            ; turn yellow west off
+          cbi       WEST, GREEN_W_PIN             ; turn green west off
+
+          ret       ; n_yellow_w_red
+n_red_w_yellow:
+          sbi       NORTH, RED_N_PIN              ; turn red north on
+          sbi       WEST, YELLOW_W_PIN            ; turn yellow west on
+          cbi       NORTH, YELLOW_N_PIN           ; turn yellow north off
+          cbi       NORTH, GREEN_N_PIN            ; turn green north off
+          cbi       WEST, RED_W_PIN               ; turn red west off
+          cbi       WEST, GREEN_W_PIN             ; turn green west off
+
+          ret       ; n_red_w_yellow
+n_red_w_red:
+          sbi       NORTH, RED_N_PIN              ; turn red north on
+          sbi       WEST, RED_W_PIN               ; turn red west on
+          cbi       NORTH, YELLOW_N_PIN           ; turn yellow north off
+          cbi       NORTH, GREEN_N_PIN            ; turn green north off
+          cbi       WEST, YELLOW_W_PIN            ; turn yellow west off
+          cbi       WEST, GREEN_W_PIN             ; turn green west off
+
+          ret       ; n_red_w_red
 
 
 
-
+; These are for crosswalk
 
 n_cross_red:
-          sbi       NORTH, CROSS_RED_N_PIN
-          cbi       NORTH, CROSS_WHITE_N_PIN
-          ret
+          sbi       NORTH, CROSS_RED_N_PIN        ; turn red north cross on
+          cbi       NORTH, CROSS_WHITE_N_PIN      ; turn white north cross off
+          ret       ; n_cross_red
 n_cross_white:
-          sbi       NORTH, CROSS_WHITE_N_PIN
-          cbi       NORTH, CROSS_RED_N_PIN
-          ret
+          sbi       NORTH, CROSS_WHITE_N_PIN      ; turn white north cross on
+          cbi       NORTH, CROSS_RED_N_PIN        ; turn red north cross off
+          ret       ; n_cross_white
 n_cross_white_off:  
-          cbi       NORTH, CROSS_WHITE_N_PIN
-          ret
+          cbi       NORTH, CROSS_WHITE_N_PIN      ; just turn off white on north cross
+          ret       ; n_cross_white_off
 
 
 w_cross_red:
@@ -245,6 +303,11 @@ w_cross_white_off:
           cbi       WEST, CROSS_WHITE_W_PIN
           ret
 
+
+; ----------------------------------------------------------
+; These are blinking functions for crosswalk
+; They can be considered as inverters
+; When white on, turn it off, when white off, turn it on
 
 n_cross_white_blink:
           sbic      NORTH, CROSS_WHITE_N_PIN
@@ -280,80 +343,19 @@ w_cross_white_blink_1:
 w_cross_white_blink_end:
           ret
 
-
-
-
-
 ; ----------------------------------------------------------
 
 
-
-n_green_w_red:
-          sbi       NORTH, GREEN_N_PIN                   ; turn green north on
-          sbi       WEST, RED_W_PIN                     ; turn red west on
-          cbi       NORTH, RED_N_PIN                     ; turn red north off
-          cbi       NORTH, YELLOW_N_PIN                  ; turn yellow north off
-          cbi       WEST, YELLOW_W_PIN                  ; turn yellow west off
-          cbi       WEST, GREEN_W_PIN                   ; turn green west off
-          
-          ret               ; return
-
-
-n_red_w_green:
-          sbi       NORTH, RED_N_PIN                     ; turn red north on
-          sbi       WEST, GREEN_W_PIN                   ; turn green west on
-          cbi       NORTH, YELLOW_N_PIN                  ; turn yellow north off
-          cbi       NORTH, GREEN_N_PIN                   ; turn green north off
-          cbi       WEST, RED_W_PIN                     ; turn red west off
-          cbi       WEST, YELLOW_W_PIN                  ; turn yellow west off
-
-          ret                     ;return
-
-
-n_yellow_w_red:
-          sbi       NORTH, YELLOW_N_PIN                  ; turn yellow north on
-          sbi       WEST, RED_W_PIN                     ; turn red west on
-          cbi       NORTH, RED_N_PIN                     ; turn red north off
-          cbi       NORTH, GREEN_N_PIN                   ; turn green north off
-          cbi       WEST, YELLOW_W_PIN                  ; turn yellow west off
-          cbi       WEST, GREEN_W_PIN                   ; turn green west off
-
-          ret      
-
-
-
-n_red_w_yellow:
-          sbi       NORTH, RED_N_PIN                     ; turn red north on
-          sbi       WEST, YELLOW_W_PIN                  ; turn yellow west on
-          cbi       NORTH, YELLOW_N_PIN                  ; turn yellow north off
-          cbi       NORTH, GREEN_N_PIN                   ; turn green north off
-          cbi       WEST, RED_W_PIN                     ; turn red west off
-          cbi       WEST, GREEN_W_PIN                   ; turn green west off
-
-          ret       
-
-n_red_w_red:
-          sbi       NORTH, RED_N_PIN                     ; turn red north on
-          sbi       WEST, RED_W_PIN                     ; turn red west on
-          cbi       NORTH, YELLOW_N_PIN                  ; turn yellow north off
-          cbi       NORTH, GREEN_N_PIN                   ; turn green north off
-          cbi       WEST, YELLOW_W_PIN                  ; turn yellow west off
-          cbi       WEST, GREEN_W_PIN                   ; turn green west off
-
-          ret       
-
-
-
-; handle decrement button press
+; handle west crosswalk button press
 ; ----------------------------------------------------------
 btn_cross_w_isr:
-          ldi       crossWFlag, 1                   ; decFlag = true
+          ldi       crossWFlag, 1       ; crossWFlag = true
 
-          reti                          ; btn_dec_isr
+          reti                          ; btn_cross_w_isr
 
-; handle increment button press
+; handle north crosswalk button press
 ; ----------------------------------------------------------
 btn_cross_n_isr:
-          ldi       crossNFlag, 1                   ; incFlag = true
+          ldi       crossNFlag, 1       ; crossNFlag = true
 
-          reti                          ; btn_inc_isr
+          reti                          ; btn_cross_n_isr
